657eafbd7fe46fbb81822925f5ec74a1f7815027
ftctechnh <ftctechnh@gmail.com>
2020-11-01 11:42:20 -0500

    Added telemetry prompt to before waitForStart().

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TomSimpleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TomSimpleOpMode.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class TomSimpleOpMode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        telemetry.addData(">>", "Press start to continue");
+        telemetry.update();
         this.waitForStart();
         if(this.opModeIsActive()) {
             while(opModeIsActive()) {

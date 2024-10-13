667507be9c89cb29a5ae093c8fe9330067c85732
Amika <92111982+Yahahama@users.noreply.github.com>
2024-10-13 13:56:55 -0400

    Continued work on still error-pocked Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/autonomous/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/autonomous/Autonomous.java
@@ -3,23 +3,31 @@ import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 @Config
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "TEST_AUTONOMOUS", group = "Autonomous")
 public class Autonomous extends LinearOpMode {
     public class Lift {
-        private DcMotorEx lift;
+        private final DcMotorEx linearSlide1;
+        private final DcMotorEx linearSlide2;
 
         public Lift(HardwareMap hardwareMap) {
-            lift = hardwareMap.get(DcMotorEx.class, "liftMotor");
-            lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            lift.setDirection(DcMotorSimple.Direction.FORWARD);
+            linearSlide1 = hardwareMap.get(DcMotorEx.class, "linearSlide1");
+            linearSlide2 = hardwareMap.get(DcMotorEx.class, "linearSlide2");
+            linearSlide1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            linearSlide2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            linearSlide1.setDirection(DcMotorSimple.Direction.FORWARD);
+            linearSlide2.setDirection(DcMotorSimple.Direction.FORWARD);
         }
 
         public class LiftUp implements Action {
@@ -28,18 +36,20 @@ public class Autonomous extends LinearOpMode {
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
                 if (!initialized) {
-                    lift.setPower(0.8);
+                    linearSlide1.setPower(0.8);
+                    linearSlide2.setPower(0.8);
                     initialized = true;
                 }
 
-                double pos = lift.getCurrentPosition();
+                double pos = linearSlide1.getCurrentPosition();  // Assumes both slides at same pos
                 packet.put("liftPos", pos);
                 if (pos < 3500.0) {  // 3500.0 is the LINEAR_SLIDE_MAX in drive.java, adjust here if adjusted elsewhere
                     // Keep raising lift if it hasn't reached max height yet
                     return true;
                 } else {
                     // If lift is at desired position, stop raising
-                    lift.setPower(0);
+                    linearSlide1.setPower(0);
+                    linearSlide2.setPower(0);
                     return false;
                 }
             }
@@ -55,7 +65,8 @@ public class Autonomous extends LinearOpMode {
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
                 if (!initialized) {
-                    lift.setPower(-0.8);
+                    linearSlide1.setPower(-0.8);
+                    linearSlide2.setPower(-0.8);
                     initialized = true;
                 }
 
@@ -75,7 +86,77 @@ public class Autonomous extends LinearOpMode {
         public Action liftDown() {
             return new LiftDown();
         }
+    }
 
+    public class Claw {
+        private Servo claw;
 
+        public Claw(HardwareMap hardwareMap) {
+            claw = hardwareMap.get(Servo.class, "claw");
+        }
+
+        public class CloseClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                claw.setPosition(0.45);  // Hardcoded from drive.java
+                return false;
+            }
+        }
+
+        public Action closeClaw() {
+            return new CloseClaw();
+        }
+
+        public class OpenClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                claw.setPosition(.75);  // Also hardcoded
+                return false;
+            }
+        }
+
+        public Action openClaw() {
+            return new OpenClaw();
+        }
+    }
+
+    public class Twist {
+        Servo twist;
+
+        public Twist(HardwareMap hardwareMap) {
+            Servo twist = hardwareMap.get(Servo.class, "twist");
+        }
+
+        public class TwistUp implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                twist.setPosition(0.0);
+                return false;
+            }
+        }
+
+        public Action twistUp() {
+            return new TwistUp();
+        }
+
+        public class TwistDown implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                twist.setPosition(0.75);
+                return false;
+            }
+        }
+
+        public Action twistDown() {
+            return new TwistDown();
+        }
+    }
+
+    @Override
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        Claw claw = new Claw(hardwareMap);
+        Lift lift = new Lift(hardwareMap);
     }
 }

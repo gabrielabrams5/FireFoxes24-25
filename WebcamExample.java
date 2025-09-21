40c6ea38f93e437a0ce45eabc8c7935f93af93e3
Bokoooooo <brooklin20201@gmail.com>
2025-09-21 14:56:44 -0400

    Integrate AprilTagDetectionPipeline into WebcamExample

--- WebcamExample.java
+++ WebcamExample.java
@@ -1,21 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
+import org.openftc.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.teamcode.pipelines.AprilTagDetectionPipeline;
+
+import java.util.ArrayList;
 
 public class WebcamExample extends LinearOpMode
 {
     OpenCvWebcam webcam;
+    AprilTagDetectionPipeline aprilTagPipeline;
 
     @Override
     public void runOpMode()
@@ -23,10 +21,11 @@ public class WebcamExample extends LinearOpMode
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
-        webcam.setPipeline(new SamplePipeline());
+        aprilTagPipeline = new AprilTagDetectionPipeline();
+        webcam.setPipeline(aprilTagPipeline);
 
         webcam.setMillisecondsPermissionTimeout(5000);
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        webcam.openCameraDeviceAsync(new OpenCvWebcam.AsyncCameraOpenListener()
         {
             @Override
             public void onOpened()
@@ -47,6 +46,12 @@ public class WebcamExample extends LinearOpMode
 
         while (opModeIsActive())
         {
+            ArrayList<AprilTagDetection> detections = aprilTagPipeline.getDetections();
+            telemetry.addData("AprilTags Detected", detections.size());
+            for (AprilTagDetection detection : detections) {
+                telemetry.addData("Tag ID", detection.id);
+                telemetry.addData("Center", detection.centre);
+            }
             telemetry.addData("Frame Count", webcam.getFrameCount());
             telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
             telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
